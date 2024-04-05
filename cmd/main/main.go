6c67b95ffb62fa4f87308885e51219c42a626673
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-04-05 16:59:31 +0000

    idk

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello crypto-pal !")
+	todecode := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	slice_to_decode := []byte(todecode)
+	dst := make([]byte, hex.DecodedLen(len(slice_to_decode)))
+	_, err := hex.Decode(dst, slice_to_decode)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%s", dst)
 }
