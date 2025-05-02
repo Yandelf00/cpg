c9b5bdc18873890faee135754912395e9b53d74c
Yandelf00 <zakaria.afyf00@gmail.com>
2025-05-03 00:42:35 +0100

    added comments

--- main.go
+++ main.go
@@ -77,9 +77,25 @@ import (
 func main() {
 
 	//testing single byte xor cipher 
-	res, _, _:= setOne.BhattacharyyaSingleByteXORCipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	// res, _, _:= setOne.BhattacharyyaSingleByteXORCipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	//
+	// fmt.Println(res)
 
-	fmt.Println(res)
+	
+	var englishFrequencies = map[rune]float64{
+		'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253,
+		'e': 12.702, 'f': 2.228, 'g': 2.015, 'h': 6.094,
+		'i': 6.966, 'j': 0.153, 'k': 0.772, 'l': 4.025,
+		'm': 2.406, 'n': 6.749, 'o': 7.507, 'p': 1.929,
+		'q': 0.095, 'r': 5.987, 's': 6.327, 't': 9.056,
+		'u': 2.758, 'v': 0.978, 'w': 2.36, 'x': 0.15,
+		'y': 1.974, 'z': 0.074, ' ': 19.0,
+	}
+	result := setOne.NormalizeFrequency(englishFrequencies)
+	for l, el := range result {
+		fmt.Println(string(l))
+		fmt.Println(el)
+	}
 
 	//testing FixedXor
 	// test, err := setOne.FixedXor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
--- setOne/bcSBXorCipher.go
+++ setOne/bcSBXorCipher.go
@@ -6,36 +6,47 @@ import (
 	"strings"
 )
 
+//Struct that holds the decrypted phrase,
+//its Bhattacharyya coefficient(score),
+//and the key with which it was decrypted.
 type BhattaRes struct {
 	phrase string
 	score  float64
 	key    byte
 }
 
+
+//This function uses bhattacharyya coefficient
+//to find the decrypted message, it takes a string input.
+//The point of this function is to detect the english sentence
+//because we don't wanna look at all the possible outputs and 
+//find the english sentence by ourselves
 func BhattacharyyaSingleByteXORCipher(input string) (string, float64, byte) {
 	var bc float64
 	bhattaRes := BhattaRes{
 		phrase: "",
 		score:  0.0,
 		key:    0,
-	}
+	} //initialization of the result
 
 	for i := 0; i <= 255; i++ {
-		res, err := FixedXorKey(input, byte(i))
+		res, err := FixedXorKey(input, byte(i)) //xor our input against every possible character
 		if err != nil {
 			log.Fatal(err)
 		}
-		bc = BhattaPhraseAnalysis(string(res))
+		bc = BhattaPhraseAnalysis(string(res)) //the function that calculates the bc for each input
 		if bc > bhattaRes.score {
 			bhattaRes.phrase = string(res)
 			bhattaRes.score = bc
 			bhattaRes.key = byte(i)
-		}
+		} //if the bc of the current phrase is higher than the one stored, we take the new one
 	}
 
 	return bhattaRes.phrase, bhattaRes.score, bhattaRes.key
 }
 
+
+//This function calculates the frequency of the letters in our decoded string (non normalized)
 func BhattaCalculateFrequency(phrase string) map[rune]float64 {
 	var frequencies = map[rune]float64{
 		'a': 0, 'b': 0, 'c': 0, 'd': 0,
@@ -45,7 +56,7 @@ func BhattaCalculateFrequency(phrase string) map[rune]float64 {
 		'q': 0, 'r': 0, 's': 0, 't': 0,
 		'u': 0, 'v': 0, 'w': 0, 'x': 0,
 		'y': 0, 'z': 0, ' ': 0,
-	}
+	} //initates the map of frequencies for our input
 	totalLetter := 0
 
 	for _, char := range strings.ToLower(phrase) {
@@ -53,18 +64,20 @@ func BhattaCalculateFrequency(phrase string) map[rune]float64 {
 			frequencies[char]++
 			totalLetter++
 		}
-	}
+	} //goes through every letter, adds the frequency if the letter is english, and add to total letters
 
 	for char := range frequencies {
 		frequencies[char] = (frequencies[char] / float64(totalLetter)) * 100
-	}
+	}//gives the percentage of each letter
 	return frequencies
 }
+
 
 func BhattaPhraseAnalysis(phrase string) float64 {
 	frequency := BhattaCalculateFrequency(phrase)
 	normalizedFrequency := NormalizeFrequency(frequency)
 	normalizedEndlishFreq := NormalizeFrequency(englishFrequencies)
+
 	bc := 0.0
 
 	for char, observed := range normalizedFrequency {
